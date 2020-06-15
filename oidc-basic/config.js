614beda27ccc435d91645bb00eab4da38dcd2ced
c-solanki <csolanki@bloksec.com>
2020-06-15 07:01:28 -0700

    Add registration page to oidc-basic

--- oidc-basic/config.js
+++ oidc-basic/config.js
@@ -4,6 +4,8 @@ const ISSUER = process.env.ISSUER || `${BLOKSEC_HOST}/oidc`;
 const CLIENT_ID = process.env.CLIENT_ID || '7af3c5b2353d48e7838fb4e0cdabad85';
 const CLIENT_SECRET = process.env.CLIENT_SECRET || 'c43f66d1a0584537b4f24791981478de';
 const BASE_URL = process.env.BASE_URL || 'http://localhost:8080';
+const READ_TOKEN = process.env.READ_TOKEN || '80b9d9d7-ab13-4e7c-82dc-53c87affe065';
+const WRITE_TOKEN = process.env.WRITE_TOKEN || '59ea3453-185c-4555-92cf-b2a487f50d3d';
 
 console.info(`BASE_URL: '${BASE_URL}'`);
 console.info(`BLOKSEC_HOST': '${BLOKSEC_HOST}'`);
@@ -19,6 +21,12 @@ module.exports = {
     clientSecret: CLIENT_SECRET,
     issuer: ISSUER,
     baseUrl: BASE_URL,
+    apiHost: BLOKSEC_HOST,
     scope: 'openid email profile'
+  },
+  secrets: {
+    readToken: READ_TOKEN,
+    writeToken: WRITE_TOKEN,
   }
 };
+
--- oidc-basic/routes/index.js
+++ oidc-basic/routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const util = require('util');
+const flash = require('express-flash');
+const axios = require('axios');
+const log4js = require('log4js');
+const config = require('../config.js');
+
+const log = log4js.getLogger('public-routes');
+log.level = 'debug';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.route(['/sign-up.html'])
+  .get((req, res) => {
+    res.render('sign-up');
+  })
+ 
+  .post(async (req, res, next) => {
+    log.debug(req.body);
+
+    const data = {
+      auth_token: config.secrets.writeToken,
+      user: {
+        name: `${req.body.firstName} ${req.body.lastName}`,
+        email: req.body.email,
+        mobile_number: req.body.mobile,
+      },
+      account: {
+        name: req.body.username,
+        appId: config.oidc.clientId,
+      },
+    }
+
+    log.debug(data);
+    const result = await axios.post(`${config.oidc.apiHost}/registration`, data);
+    log.debug(result);
+    res.render('sign-upsplash', {
+      username: req.body.username
+    });
+  });
+
+
 
 module.exports = router;
